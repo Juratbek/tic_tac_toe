d2728405a578e667f781ac37a65f1253d167fae3
juratbek <juratbektfi1998@gmail.com>
2024-08-23 11:27:46 +0500

    main:moving codes into src folder

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import { gameBoxClickHandler, startNewRound } from "./handlers";
-import { renderGameBoxContent, renderScoresBoxContent } from "./renderer";
-import { generateEmptyCoords } from './utils';
+import { gameBoxClickHandler, startNewRound } from "./src/handlers";
+import { renderGameBoxContent, renderScoresBoxContent } from "./src/renderer";
+import { generateEmptyCoords } from "./src/utils";
 
-import './config.css';
-import './index.css';
+import "./config.css";
+import "./index.css";
 
 window.onload = () => {
   const gameContainer = document.querySelector("#game-container");
   gameContainer.addEventListener("click", gameBoxClickHandler);
 
-  const playAgainBtn = document.querySelector('#play-again-btn');
-  playAgainBtn.addEventListener('click', startNewRound)
+  const playAgainBtn = document.querySelector("#play-again-btn");
+  playAgainBtn.addEventListener("click", startNewRound);
 
   renderScoresBoxContent();
-  const emptyCoords = generateEmptyCoords()
+  const emptyCoords = generateEmptyCoords();
   renderGameBoxContent(emptyCoords);
 };
