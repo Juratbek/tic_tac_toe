4e67567dd1e847a5b8a842cca4b2e8526d85048a
juratbek <juratbektfi1998@gmail.com>
2024-08-24 16:12:04 +0500

    main:adding webpack-copy-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './index.js',
+  entry: "./index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    assetModuleFilename: "images/[hash][ext][query]",
   },
-  plugins: [new HtmlWebpackPlugin({ template: './index.html' })],
+  plugins: [
+    new HtmlWebpackPlugin({ template: "./index.html" }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public", to: "public" }],
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.css/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
